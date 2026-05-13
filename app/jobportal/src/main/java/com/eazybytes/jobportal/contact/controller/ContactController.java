e4c6ea47a3fff7bf90280d7ea1f18339a02f9d46
Suman Yadav <sumanydv615@gmail.com>
2026-05-12 23:52:19 -0500

    (backend): implmenting exception handling and validation

--- app/jobportal/src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
+++ app/jobportal/src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
@@ -2,21 +2,26 @@ package com.eazybytes.jobportal.contact.controller;
 
 import com.eazybytes.jobportal.contact.service.IContactService;
 import com.eazybytes.jobportal.dto.ContactRequestDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contacts")
 @RequiredArgsConstructor
 public class ContactController {
+
     private final IContactService contactService;
+
+
     @PostMapping(version = "1.0")
-    public ResponseEntity<String> saveContactMsg(@RequestBody ContactRequestDto contactRequestDto) {
+    public ResponseEntity<String> saveContactMsg(@RequestBody @Valid ContactRequestDto contactRequestDto) {
+
         boolean isSaved =  contactService.saveContact(contactRequestDto);
         if (isSaved) {
             return ResponseEntity.status(HttpStatus.CREATED)
@@ -25,5 +30,14 @@ public class ContactController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Request processing failed");
         }
+    }
+
+    @GetMapping(version = "1.0")
+    public ResponseEntity<String> fetchOpenContact(@RequestParam
+                                                       @Validated
+                                                       @NotBlank(message = "status can't be blank")
+                                                       @Size(min = 3, max = 10, message = "status must be between 3 and 10 characters")
+                                                       String status){
+        return ResponseEntity.ok().body("Open Contacts : " + status);
     }
 }
--- app/jobportal/src/main/java/com/eazybytes/jobportal/dto/ContactRequestDto.java
+++ app/jobportal/src/main/java/com/eazybytes/jobportal/dto/ContactRequestDto.java
@@ -1,7 +1,32 @@
 package com.eazybytes.jobportal.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
 
-public record ContactRequestDto(String email, String message, String name, String subject,
-                                String userType) implements Serializable {
+public record ContactRequestDto(
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Message is required")
+        @Size(min = 10, max=500, message = "Message must be between 10 and 500 characters")
+        String message,
+
+        @NotBlank(message = "Name is required")
+        @Size(min = 3, max=50, message = "Name must be between 3 and 50 characters")
+        String name,
+
+        @NotBlank(message = "Subject is required")
+        @Size(min = 3, max=150, message = "Subject must be between 3 and 150 characters")
+        String subject,
+
+        @NotBlank(message = "User Type is required")
+        @Pattern(regexp = "Employer|Job Seeker|Other", message = "Invalid user type")
+        String userType)
+        implements Serializable {
 }

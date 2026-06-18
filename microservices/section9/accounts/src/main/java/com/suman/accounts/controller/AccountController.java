f7bada617f7e6ba88704a7a8bd08a8974c47d680
Suman Yadav <sumanydv615@gmail.com>
2026-06-18 10:38:20 -0500

    implemented circuit breaker , http timeout, and retry pattern to make microservice resilient

--- microservices/section9/accounts/src/main/java/com/suman/accounts/controller/AccountController.java
+++ microservices/section9/accounts/src/main/java/com/suman/accounts/controller/AccountController.java
@@ -103,5 +103,4 @@ public class AccountController {
                 .body(accountsContactInfoDto);
     }
 
-
 }
--- microservices/section9/gatewayserver/src/main/java/com/suman/gatewayserver/GatewayserverApplication.java
+++ microservices/section9/gatewayserver/src/main/java/com/suman/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -20,7 +22,9 @@ public class GatewayserverApplication {
 		return routeLocatorBuilder.routes()
 				.route(r -> r.path("/smallbank/accounts/**")
 						.filters(f -> f.rewritePath("/smallbank/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
 						.uri("lb://ACCOUNTS"))
 				.route(r -> r.path("/smallbank/cards/**")
 						.filters(f -> f.rewritePath("/smallbank/cards/(?<segment>.*)", "/${segment}")
@@ -28,7 +32,10 @@ public class GatewayserverApplication {
 						.uri("lb://CARDS"))
 				.route(r -> r.path("/smallbank/loans/**")
 						.filters(f -> f.rewritePath("/smallbank/loans/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
 						.uri("lb://LOANS"))
 				.build();
 	}
